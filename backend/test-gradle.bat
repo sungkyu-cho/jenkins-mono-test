64527acfe1e60c6fc337b6796163bd5b4d2a2f75
sgc5531 <sgc5531@t-wise.co.kr>
2025-07-21 09:35:42 +0900

    error correction

--- backend/test-gradle.bat
+++ backend/test-gradle.bat
@@ -8,15 +8,21 @@ set GRADLE_USER_HOME=C:\gradle-home
 echo GRADLE_USER_HOME: %GRADLE_USER_HOME%
 
 echo.
-echo 2. Java 버전 확인...
+echo 2. 디렉토리 생성...
+if not exist "C:\gradle-home" mkdir "C:\gradle-home"
+if not exist "C:\gradle-home\wrapper" mkdir "C:\gradle-home\wrapper"
+if not exist "C:\gradle-home\wrapper\dists" mkdir "C:\gradle-home\wrapper\dists"
+
+echo.
+echo 3. Java 버전 확인...
 java -version
 
 echo.
-echo 3. Gradle Wrapper 버전 확인...
+echo 4. Gradle Wrapper 버전 확인...
 gradlew.bat --version
 
 echo.
-echo 4. 빌드 테스트...
+echo 5. 빌드 테스트...
 gradlew.bat clean build
 
 echo.
